17f70e606d197ce713ec15e215bced77c08e9a98
Dccreswe <77848803+Dccreswe@users.noreply.github.com>
2021-02-05 18:57:11 -0500

    AddRel and DelRel
    We have coded these methods. They have not been tested.

--- src/main/java/edu/millersville/uml_editor/Relationships.java
+++ src/main/java/edu/millersville/uml_editor/Relationships.java
@@ -5,12 +5,14 @@ import java.util.*;
 public class Relationships {
 
 	private Class source;
-	private Class destination ;
+	private Class destination;
+	private int ID;
 
 	
-	public void Relationships(Class class1, Class class2) {
+	public Relationships(Class class1, Class class2, int IDD) {
 		source = class1;
 		destination = class2;
+		ID = IDD;
 	}
 
 }
