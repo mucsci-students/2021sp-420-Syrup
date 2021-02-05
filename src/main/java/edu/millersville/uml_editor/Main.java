6c8fe67dac92a2bf337465b130f33d3edd6cfdfa
Dccreswe <77848803+Dccreswe@users.noreply.github.com>
2021-02-05 17:13:43 -0500

    Update Relationships.java
    Created the Relationship class.

--- src/main/java/edu/millersville/uml_editor/Main.java
+++ src/main/java/edu/millersville/uml_editor/Main.java
@@ -66,6 +66,23 @@ public class Main
     	
     	classMap.put(className, new Class());
     }
+
+///////////////////////////////////////////////////////////
+//
+//	deleteClass
+//
+///////////////////////////////////////////////////////////
+
+    public static void deleteClass(class name){
+
+        if (!classMap.containsKey(name))
+    	{
+    		System.out.println("There is not a class with that name.");
+    		return;
+    	}
+        classMap.remove(name);
+    }
+
     
 ///////////////////////////////////////////////////////////
 //
