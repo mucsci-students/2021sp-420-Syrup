718e6e20482f0e5defcde45c8ed7221a442cd888
ajphilli <62962737+ajphilli@users.noreply.github.com>
2021-02-14 13:23:57 -0500

    Update Main.java

--- src/main/java/edu/millersville/uml_editor/Main.java
+++ src/main/java/edu/millersville/uml_editor/Main.java
@@ -60,7 +60,7 @@ public class Main
 //
 ///////////////////////////////////////////////////////////
 	
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
         boolean loop = true;
         while(loop)
