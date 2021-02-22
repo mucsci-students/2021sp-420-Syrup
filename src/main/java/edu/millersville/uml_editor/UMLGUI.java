9a49c6557d401a88c3397ddab86500745e5a274f
Dccreswe <77848803+Dccreswe@users.noreply.github.com>
2021-02-21 23:19:30 -0500

    Update UMLGUI.java
    Added the menu bar with menu options being save and load.

--- src/main/java/edu/millersville/uml_editor/UMLGUI.java
+++ src/main/java/edu/millersville/uml_editor/UMLGUI.java
@@ -6,6 +6,9 @@ import java.awt.event.ActionListener;
 public class UMLGUI {
 
         public static void main(String[] args) {
+            ///////////////
+            // Heading/Labels
+            ///////////////
             //creates the first line of text and changes the font
             JLabel welcome = new JLabel("Welcome to our UML Editor!", SwingConstants.CENTER);
             welcome.setFont(new Font("Serif", Font.BOLD, 20));
@@ -19,41 +22,48 @@ public class UMLGUI {
             ///////////////
             // MENU
             ///////////////
-            JMenuBar umlMenu = new JMenuBar();
+            JMenuBar menuBar = new JMenuBar();
+            JMenu menu = new JMenu("File");
+            JMenuItem save = new JMenuItem ("Save");
+            JMenuItem load = new JMenuItem ("Load");
+            menu.add(save);
+            menu.add(load);
+            menuBar.add(menu);
 
-            //create all the buttons of main menu
+            ///////////////
+            // Buttons
+            ///////////////
             JButton classButton = new JButton("Class");
             JButton attrButton = new JButton("Attribute");
             JButton relButton = new JButton("Relationship");
-            JButton loadButton = new JButton("Load");
-            JButton saveButton = new JButton("Save");
             JButton printButton = new JButton("Print");
             JButton helpButton = new JButton("Help");
 
-
-
-            JFrame mm = new JFrame("UML Editor");
-            mm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-            JPanel menuPanel = new JPanel(new GridLayout(10, 1, 8, 8));
-
+            ///////////////
+            // Panel
+            ///////////////
+            JPanel menuPanel = new JPanel(new GridLayout(8, 1, 8, 8));
             //adds everything to the panel
             menuPanel.add(welcome);
             menuPanel.add(select);
             menuPanel.add(classButton);
             menuPanel.add(attrButton);
             menuPanel.add(relButton);
-            menuPanel.add(loadButton);
-            menuPanel.add(saveButton);
             menuPanel.add(printButton);
             menuPanel.add(helpButton);
+            menuPanel.setBorder(BorderFactory.createEmptyBorder(10, 8, 10, 8));
 
-            menuPanel.setBorder(BorderFactory.createEmptyBorder(20, 8, 20, 8));
-            
-            mm.add(umlMenu);
+            ///////////////
+            // Frame
+            ///////////////
+            JFrame mm = new JFrame("UML Editor");
+            mm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            mm.setJMenuBar(menuBar);
             mm.setContentPane(menuPanel);
             mm.setSize(400, 600);
             mm.setVisible(true);
-            umlMenu.setVisible(true);
+
         }
     }
+
+
