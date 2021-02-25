690024558d3794a0ec41be0cd6b99b880d2f1058
ajphilli <62962737+ajphilli@users.noreply.github.com>
2021-02-25 13:02:53 -0500

    Update Relationships.java

--- src/main/java/edu/millersville/uml_editor/Relationships.java
+++ src/main/java/edu/millersville/uml_editor/Relationships.java
@@ -1,4 +1,3 @@
-package edu.millersville.uml_editor;
 
 import java.util.*;
 
@@ -7,6 +6,7 @@ public class Relationships {
 	private Class source;
 	private Class destination;
 	private static String ID;
+	private String type;
 
 ///////////////////////////////////////////////////////////
 //
@@ -14,11 +14,12 @@ public class Relationships {
 //
 ///////////////////////////////////////////////////////////
 
-	public Relationships(Class class1, Class class2, String id) 
+	public Relationships(Class class1, Class class2, String id, String type1) 
 	{
 		source = class1;
 		destination = class2;
 		ID = id;
+		type = type1;
 	}
 	
 ///////////////////////////////////////////////////////////
@@ -41,5 +42,27 @@ public class Relationships {
 	public String destinationName()
 	{
 		return destination.getClassName();
+	}
+
+///////////////////////////////////////////////////////////
+//
+//	changeType
+//
+///////////////////////////////////////////////////////////
+
+	public void changeType(String newType)
+	{
+		type = newType;
+	}
+
+///////////////////////////////////////////////////////////
+//
+//	relType
+//
+///////////////////////////////////////////////////////////
+
+	public String relType()
+	{
+		return type;
 	}
 }
